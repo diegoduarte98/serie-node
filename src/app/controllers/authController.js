cffa9c9023c96ec63043d2008237bd6b8b795007
Diego Duarte <diegoduarte98@gmail.com>
2018-08-24 19:19:10 -0300

    reset password

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -75,15 +75,43 @@ router.post('/forgot_password', async (req, res) => {
             context: { token }
         }, (err) => {
             console.log(err);
-            if(err)
-                return res.status(400).send({ error: 'Cannot send forgot password'});
-            
+            if (err)
+                return res.status(400).send({ error: 'Cannot send forgot password' });
+
             return res.send();
         });
 
     } catch (err) {
         console.log(err);
         res.status(400).send({ error: 'Erro on forgot password, try again' });
+    }
+});
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires');
+
+        if (!user)
+            return res.status(400).send({ error: 'User not found' });
+
+        if(token !== user.passwordResetToken)
+            return res.status(400).send({ error : 'Token invalid' });
+
+        const now = new Date();
+
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({ error : 'Token expired, generate a new one' });
+
+        user.password = password;
+        
+        await user.save();
+
+        res.send();
+    } catch (err) {
+        res.status(400).send({ error: 'Cannot reset password, try again' })
     }
 });
 
